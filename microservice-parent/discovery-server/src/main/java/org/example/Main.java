6e01263ddc798ac343847b6cefef5edef5e61929
pthienquan1 <quan01011997123@gmail.com>
2023-06-12 18:53:10 +0700

    add discovery-server and dependency about eureka

---
+++ microservice-parent/discovery-server/src/main/java/org/example/Main.java
@@ -0,0 +1,12 @@
+package org.example;
+
+/**
+ * Created by ${USER}
+ * Date ${DATE} - ${TIME}
+ * Description: ...
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}

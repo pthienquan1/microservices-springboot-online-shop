cd8ffb820c78747db9944894523f556567e420eb
pthienquan1 <quan01011997123@gmail.com>
2023-06-13 20:51:17 +0700

    Init api-gateway module

---
+++ microservice-parent/api-gateway/src/main/java/com/pthienquan1/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,17 @@
+package com.pthienquan1.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Created by quan0
+ * Date 6/13/2023 - 8:46 PM
+ * Description: ...
+ */
+@SpringBootApplication
+public class ApiGatewayApplication {
+    public static void main(String[] args){
+        SpringApplication.run(ApiGatewayApplication.class,args);
+
+    }
+}
